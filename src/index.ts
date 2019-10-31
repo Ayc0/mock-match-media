a32cfa717baab4ca30068b4fc0470790f2de4b9a
Ayc0 <ayc0.benj@gmail.com>
2019-10-31 16:42:04 -0400

    add setMedia

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { match, parse, Feature } from "css-mediaquery";
+import { match, parse, Feature, MediaState } from "css-mediaquery";
+
+const state: Partial<MediaState> = {};
+const MEDIA = Symbol("MEDIA");
+const PREVIOUS_MATCH = Symbol("PREVIOUS_MATCH");
 
 const getFeaturesFromQuery = (query: string) => {
     const parsedQuery = parse(query);
@@ -12,10 +16,10 @@ const getFeaturesFromQuery = (query: string) => {
 };
 
 type Listener = (event: MediaQueryListEvent) => void;
-
 const listeners = new Map<Feature, Set<Listener>>();
 
 const addListener = (query: string, callback: Listener) => {
+    callback[MEDIA] = query;
     const features = getFeaturesFromQuery(query);
     features.forEach(feature => {
         if (!listeners.has(feature)) {
@@ -39,29 +43,86 @@ const removeListener = (query: string, callback: Listener) => {
     });
 };
 
-const state: Record<string, string> = {};
+export const matchMedia: typeof window.matchMedia = (query: string) => {
+    const matches = match(query, state);
+    return {
+        matches,
+        media: query,
+        onchange: () => {
+            throw new Error("not supported");
+        },
+        addEventListener: (event, callback) => {
+            if (event !== "change") {
+                return;
+            }
+            callback[PREVIOUS_MATCH] = matches;
+            addListener(query, callback);
+        },
+        removeEventListener: (event, callback) => {
+            if (event !== "change") {
+                return;
+            }
+            removeListener(query, callback);
+        },
+        dispatchEvent: () => {
+            throw new Error("not supported");
+        },
+        addListener: callback => {
+            callback[PREVIOUS_MATCH] = matches;
+            addListener(query, callback);
+        },
+        removeListener: callback => removeListener(query, callback),
+    };
+};
 
-export const matchMedia: typeof window.matchMedia = (query: string) => ({
-    matches: match(query, state),
-    media: query,
-    onchange: () => {
-        throw new Error("not supported");
-    },
-    addEventListener: (event, callback) => {
-        if (event !== "change") {
+const defaultEvent = {
+    type: "match-media",
+    bubbles: false,
+    cancelBubble: false,
+    cancelable: false,
+    composed: false,
+    target: null,
+    currentTarget: null,
+    defaultPrevented: false,
+    eventPhase: 0,
+    isTrusted: false,
+    initEvent: () => {},
+    AT_TARGET: null,
+    BUBBLING_PHASE: null,
+    CAPTURING_PHASE: null,
+    NONE: null,
+    composedPath: () => [],
+    preventDefault: () => {},
+    stopImmediatePropagation: () => {},
+    stopPropagation: () => {},
+    returnValue: true,
+    srcElement: null,
+};
+
+export const setMedia = (media: Partial<MediaState>) => {
+    const changedFeatures = new Set<Feature>();
+    Object.keys(media).forEach(feature => {
+        changedFeatures.add(feature);
+        state[feature] = media[feature];
+    });
+    changedFeatures.forEach(changedFeature => {
+        const activeListeners = listeners.get(changedFeature);
+        if (!activeListeners) {
             return;
         }
-        addListener(query, callback);
-    },
-    removeEventListener: (event, callback) => {
-        if (event !== "change") {
-            return;
-        }
-        removeListener(query, callback);
-    },
-    dispatchEvent: () => {
-        throw new Error("not supported");
-    },
-    addListener: callback => addListener(query, callback),
-    removeListener: callback => removeListener(query, callback),
-});
+        activeListeners.forEach(listener => {
+            const query = listener[MEDIA];
+            const matches = match(query, state);
+            const previousMatches = listener[PREVIOUS_MATCH];
+            if (previousMatches !== matches) {
+                listener({
+                    ...defaultEvent,
+                    timeStamp: Date.now(),
+                    matches,
+                    media: query,
+                });
+            }
+            listener[PREVIOUS_MATCH] = matches;
+        });
+    });
+};
