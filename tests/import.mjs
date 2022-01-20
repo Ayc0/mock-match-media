22e4d5a296be37d81b981bbbb3b53e3d1c3be7ef
Ayc0 <ayc0.benj@gmail.com>
2022-01-20 16:54:28 +0100

    add tests

---
+++ tests/import.mjs
@@ -0,0 +1,17 @@
+import test from "ava";
+
+test("can import mock-match-media from ESM", async (t) => {
+    const exportsDefault = await import("mock-match-media");
+    t.deepEqual(Object.keys(exportsDefault), ["cleanup", "cleanupListeners", "cleanupState", "matchMedia", "setMedia"]);
+    t.pass();
+});
+
+test("can import mock-match-media/polyfill from ESM", async (t) => {
+    t.is(global.matchMedia, undefined);
+    const { matchMedia } = await import("mock-match-media");
+
+    await import("mock-match-media/polyfill");
+
+    t.is(global.matchMedia, matchMedia);
+    t.pass();
+});
---
+++ tests/listeners.cjs
@@ -0,0 +1,107 @@
+const test = require("ava");
+const { matchMedia, setMedia, cleanupListeners, cleanupState, cleanup } = require("mock-match-media");
+
+const waitFor = async (cb) => {
+    await cb().catch(() => {
+        return new Promise((res, rej) => waitFor(cb).then(res).catch(rej), 10);
+    });
+};
+
+test.afterEach(() => {
+    // cleanup listeners and state after each test
+    cleanup();
+});
+
+test(".addListener()", async (t) => {
+    const mql = matchMedia("(min-width: 500px)");
+
+    const calls = [];
+    const cb = (event) => {
+        calls.push(event);
+    };
+
+    mql.addListener(cb);
+
+    setMedia({
+        width: "600px",
+    });
+    t.is(calls.length, 1);
+    t.is(calls[0].matches, true);
+
+    setMedia({
+        width: "300px",
+    });
+    t.is(calls.length, 2);
+    t.is(calls[1].matches, false);
+
+    // No new call
+    setMedia({
+        width: "200px",
+    });
+    t.is(calls.length, 2);
+
+    // cleanup make it so that new changes in the media won't trigger listeners
+    cleanupListeners();
+    setMedia({
+        width: "600px",
+    });
+    t.is(calls.length, 2);
+
+    // same thing for cleanup
+    mql.addListener(cb);
+    cleanup();
+
+    setMedia({
+        width: "300px",
+    });
+    t.is(calls.length, 2);
+
+    t.pass();
+});
+
+test(".addEventListener()", async (t) => {
+    const mql = matchMedia("(min-width: 500px)");
+
+    const calls = [];
+    const cb = (event) => {
+        calls.push(event);
+    };
+
+    mql.addEventListener("change", cb);
+
+    setMedia({
+        width: "600px",
+    });
+    t.is(calls.length, 1);
+    t.is(calls[0].matches, true);
+
+    setMedia({
+        width: "300px",
+    });
+    t.is(calls.length, 2);
+    t.is(calls[1].matches, false);
+
+    // No new call
+    setMedia({
+        width: "200px",
+    });
+    t.is(calls.length, 2);
+
+    // cleanup make it so that new changes in the media won't trigger listeners
+    cleanupListeners();
+    setMedia({
+        width: "600px",
+    });
+    t.is(calls.length, 2);
+
+    // same thing for cleanup
+    mql.addListener(cb);
+    cleanup();
+
+    setMedia({
+        width: "300px",
+    });
+    t.is(calls.length, 2);
+
+    t.pass();
+});
