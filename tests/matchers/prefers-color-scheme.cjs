12ad21b0496d70e1498e2b5ce7a3420fe69461d7
Ayc0 <ayc0.benj@gmail.com>
2025-05-24 20:25:42 +0200

    add tests for prefers-color-scheme

---
+++ tests/matchers/prefers-color-scheme.cjs
@@ -0,0 +1,37 @@
+// @ts-check
+
+const test = require("ava");
+const { matchMedia, setMedia, cleanupMedia } = require("mock-match-media");
+
+test.beforeEach(() => {
+    cleanupMedia();
+});
+
+test.serial("unset", (t) => {
+    t.is(matchMedia("(prefers-color-scheme: light)").matches, false);
+    t.is(matchMedia("(prefers-color-scheme: dark)").matches, false);
+
+    t.pass();
+});
+
+test.serial("light", (t) => {
+    setMedia({
+        prefersColorScheme: "light",
+    });
+
+    t.is(matchMedia("(prefers-color-scheme: light)").matches, true);
+    t.is(matchMedia("(prefers-color-scheme: dark)").matches, false);
+
+    t.pass();
+});
+
+test.serial("dark", (t) => {
+    setMedia({
+        prefersColorScheme: "dark",
+    });
+
+    t.is(matchMedia("(prefers-color-scheme: light)").matches, false);
+    t.is(matchMedia("(prefers-color-scheme: dark)").matches, true);
+
+    t.pass();
+});
