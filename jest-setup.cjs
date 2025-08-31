fcc98bac1ca3df6bdfdd91753d8ef56fea4373c0
Benjamin <ayc0.benj@gmail.com>
2025-08-31 09:08:26 +0200

    add TextEncoder is Jest setup (#60)

--- jest-setup.cjs
+++ jest-setup.cjs
@@ -1,3 +1,9 @@
+// The library depends on TextEncoder, but JSDOM doesn’t include it (see https://github.com/jsdom/jsdom/issues/2524)
+if (typeof globalThis.TextEncoder === "undefined") {
+    const { TextEncoder } = require("node:util");
+    Object.assign(globalThis, { TextEncoder });
+}
+
 const { cleanup } = require("./dist/index.js");
 
 require("./polyfill.cjs");
